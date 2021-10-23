88382c032aa0cd13f663e95653b22c275850e853
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-10-23 15:41:47 +0530

    OddnEven added

---
+++ src/OddEven.java
@@ -0,0 +1,14 @@
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		int num = 7;
+		
+	if(num%2==0)
+		System.out.println(num+" is a Even number");
+	else
+		System.out.println(num+" is a Odd number");
+
+	}
+
+}
