10c0425333f3fbfbce817ed49bcc62e31fcbe875
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-10-23 15:54:09 +0530

    windChill added

---
+++ src/WindChill.java
@@ -0,0 +1,10 @@
+
+public class WindChill {
+    public static void main(String[] args) {
+        double t = 40; // temperature
+        double v = 50; // wind speed (mph)
+        double windChill = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
+        System.out.println("Given a temperature of " + t + " Fahrenheit" + " and a wind speed of " + v + " mph");
+        System.out.println("The wind chill is " + windChill);
+    }
+}
